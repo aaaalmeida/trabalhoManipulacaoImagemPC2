8616e654063cfcb604805014fb7a02826945a2e0
aaaalmeida <89162654+aaaalmeida@users.noreply.github.com>
2022-05-14 17:24:00 -0300

    Update main.c

--- main.c
+++ main.c
@@ -15,16 +15,10 @@
     NOTE QUE ESSA CODIFICAÇÃO GARANTE VALORES LBP SOMENTE ENTRE 0 e 255.
 */
 
-/*  
-    problemas do código até agora: 
-    1 - não sei como ler comentário no arquivo original (if com condição igual a # ????)
-    2 - implementar a variante P5 pra ganhar 3pontos extras
-    3 - o código atualmente so consegue executar perfeitamente com o exemplo.pgm 
-    (achoq o problema pra ler outros arquivos esta nas quebra de linha da matriz original)
-*/
-
 int calcula_ponta(int referencia, int pixel_referencia, int v1, int v2, int v3)
 {
+    // 4PIXELS
+
     // CONDIÇÃO 1: VARIAVEL ESTA NA PONTA DA MATRIZ
     // (soma 3casas)
     int valorPixel=0;
@@ -62,6 +56,8 @@ int calcula_ponta(int referencia, int pixel_referencia, int v1, int v2, int v3)
 
 int calcula_beirada(int referencia, int pixel_referencia, int v1, int v2, int v3, int v4, int v5)
 {
+    // LINHA*2 + COLUNA*2 - 4
+
     // CONDIÇÃO 2: VARIAVEL NA BEIRADA MAS NÃO NA PONTA
     // (soma 5casas)
     int valorPixel=0;
@@ -106,7 +102,10 @@ int calcula_beirada(int referencia, int pixel_referencia, int v1, int v2, int v3
 }
 
 int calcula_meio(int pixel_referencia, int v1, int v2, int v3, int v4, int v5, int v6, int v7, int v8)
-{
+{   
+    // LINHA * COLUNA - VALOR CALCULADO NAS OUTRAS FUNÇÕES
+    // OU (LINHA - 2) * (COLUNA - 2)
+
     // CONDIÇÃO 3: VARIAVEL NO MEIO DA MATRIZ
     // (soma 8casas)
     int valorPixel=0;
@@ -123,6 +122,7 @@ int calcula_meio(int pixel_referencia, int v1, int v2, int v3, int v4, int v5, i
 }
 
 void testeArquivo(FILE *arquivo){
+    
     if(!arquivo){
         printf("ERRO AO ABRIR ARQUIVO");
         exit(1);
@@ -132,36 +132,54 @@ void testeArquivo(FILE *arquivo){
 int main()
 {
     FILE *arquivo;
-    arquivo = fopen("exemplo.pgm", "r");
+    arquivo = fopen("mona_lisa.ascii.pgm", "r");
     
     // função para teste de leitura do arquivo
     testeArquivo(arquivo);
     
     // criação de variaveis 
     char tipo[3];
-    int linha, coluna, MAX_ORIGINAL;
+    int linha, coluna;
+    int MAX_ORIGINAL;   
+    // MAX_ORIGINAL não serve pra nada no codigo porque vMAX é uma constante quando usamos o filtro LBP
 
     // vMAX é uma constante em todos arquivos no filtro LBP porque (2^8) - 1 == 255
     const int vMAX = 255;
 
     // leitura de variaveis
     fscanf(arquivo, "%s", &tipo);
-    printf("\ntipo: %s", tipo);
     tipo[3] = '\0';
     fscanf(arquivo, "%d %d", &coluna, &linha);
-    printf("\ncoluna: %d linha: %d", coluna, linha);
     fscanf(arquivo, "%d", &MAX_ORIGINAL);
-    printf("\nMAX ORIGINAL: %d", MAX_ORIGINAL);
 
-    // criação dos indices e matrizes original e copia
-    int Orig[linha][coluna], Copia[linha][coluna];      // dps mudar variavel copia para um ponteiro
+    /*
+     As duas matrizes são do tipo unsigned char para economizar memoria.
+     Criando as matrizes do tipo inteiro inicialmente funciona com matrizes pequenas
+     porque o programa não aloca tanto espaço.
+     O problema ocorre quando usamos imagens muito grandes, consumindo ( coluna*linha*4 ) bytes.
+     Usando unsigned char consegui diminuir esse número 4 vezes.
+    */
+    unsigned char Orig[linha][coluna];  
+    unsigned char *Copia = malloc( linha * coluna * sizeof(char));
+    // Único motivo para Copia ser um ponteiro é para poder mostrar a matriz no terminal.
+    // O programa funcionaria igual se fosse uma variavel char primitiva para coleta e atribuição direta.
+
+    if(!Copia){
+        printf("\nNAO CONSEGUI ALOCAR O PONTEIRO PARA A MATRIZ");
+        exit(1);
+    }
+    
+    // ponteiro com o endereço original da matriz copia
+    char *comecoCopia = Copia;
+    
+    // criação dos índices
     int i,j;
 
     /*
-        variavel referencia serve como parametro na definição do pixel nas condições 1 e 2
-        funciona com base no teclado númerico
+     variavel referencia serve como parametro na definição do pixel nas condições 1 e 2,
+     funciona com base no teclado númerico
     */
-    int referencia=0;   
+    int referencia;   
 
     // leitura da matriz original
     for(i=0; i<linha; i++)
@@ -171,25 +189,35 @@ int main()
             fscanf(arquivo, "%d ", &Orig[i][j]);
         }
     }
-    printf("\n");
-    
-    // escrita da matriz original no termminal (opcional)
-    for(i=0; i<linha; i++)
+
+    int mostraNoTerminal = 0; // variável opcional para mostrar as matrizes no terminal
+    if(mostraNoTerminal)    // mostrando a matriz original no terminal
     {
-        for(j=0; j<coluna; j++) 
+        printf("\ntipo: %s", tipo);
+        printf("\ncoluna: %d linha: %d", coluna, linha);
+        printf("\nMAX ORIGINAL: %d\n", MAX_ORIGINAL);
+        for(i=0; i<linha; i++)
         {
-            printf("%d ", Orig[i][j]);
+            for(j=0; j<coluna; j++) 
+            {
+                printf("%d ", Orig[i][j]);
+            }
+            printf("\n");
         }
-        printf("\n");
     }
 
     // fechando o arquivo
     fclose(arquivo);
     // criando um novo arquivo
-    arquivo = fopen("exemplo_copia.pgm", "w+");
+    arquivo = fopen("monalisa_copia.pgm", "w");
 
     // teste de leitura do arquivo
     testeArquivo(arquivo);
+
+    // escrita da variante, linhas e colunas e valor maximo do pixel no arquivo.pgm
+    fprintf(arquivo, "%s\n", tipo);
+    fprintf(arquivo, "%d %d\n", coluna, linha);
+    fprintf(arquivo, "%d\n", vMAX);
 
     // preenchimento da matriz
     for(i=0; i<linha; i++)
@@ -198,97 +226,96 @@ int main()
         {
             // DIVIDIDO EM 3 CONDIÇÕES
 
-
             // CONDIÇÃO 1: VARIAVEL ESTA NA PONTA DA MATRIZ
-            if((i==0||i==linha-1) && (j==0||j==coluna-1))
+            if( (i==0||i==linha-1) && (j==0||j==coluna-1) )
             {
                 if(i==0&&j==0)                     //SUPERIOR ESQUERDA
                 {
                     referencia = 7;
-                    Copia[i][j] = calcula_ponta( referencia, Orig[i][j], Orig[i][j+1], Orig[i+1][j], Orig[i+1][j+1]);
+                    *Copia = calcula_ponta( referencia,Orig[i][j],Orig[i][j+1],Orig[i+1][j],Orig[i+1][j+1]);
                 }
                 else if(i==0&&j==coluna-1)         //SUPERIOR DIREITA
                 {
                     referencia = 9;
-                    Copia[i][j] = calcula_ponta( referencia, Orig[i][j],Orig[i][j-1],Orig[i+1][j-1],Orig[i+1][j]);
+                    *Copia = calcula_ponta( referencia,Orig[i][j],Orig[i][j-1],Orig[i+1][j-1],Orig[i+1][j]);
                 }
                 else if(i==linha-1&&j==0)          //INFERIOR ESQUERDA
                 {
                     referencia = 1;
-                    Copia[i][j] = calcula_ponta( referencia, Orig[i][j],Orig[i-1][j],Orig[i-1][j+1],Orig[i][j+1]);
+                    *Copia = calcula_ponta( referencia,Orig[i][j],Orig[i-1][j],Orig[i-1][j+1],Orig[i][j+1]);
                 }
                 else if(i==linha-1&&j==coluna-1)   //INFERIOR DIREITA
                 {
                     referencia = 3;
-                    Copia[i][j] = calcula_ponta( referencia, Orig[i][j],Orig[i-1][j-1],Orig[i-1][j],Orig[i][j-1]);
+                    *Copia = calcula_ponta( referencia,Orig[i][j],Orig[i-1][j-1],Orig[i-1][j],Orig[i][j-1]);
                 }
             }
 
             // CONDIÇÃO 2: VARIAVEL NA BEIRADA MAS NÃO NA PONTA
-            else if((i==0||i==linha-1) || (j==0||j==coluna-1))
+            else if( (i==0||i==linha-1) || (j==0||j==coluna-1) )
                 {
                 if(i==0)                            //LINHA DE CIMA
                 {
                     referencia = 8;
-                    Copia[i][j]=calcula_beirada(referencia,Orig[i][j],Orig[i][j-1],Orig[i][j+1],Orig[i+1][j-1],Orig[i-1][j],Orig[i+1][j+1]);
+                    *Copia=calcula_beirada(referencia,Orig[i][j],Orig[i][j-1],Orig[i][j+1],Orig[i+1][j-1],Orig[i-1][j],Orig[i+1][j+1]);
                 }
                 else if(i==linha-1)                 //LINHA DE BAIXO
                 {
                     referencia = 2;
-                    Copia[i][j] = calcula_beirada(referencia, Orig[i][j], Orig[i-1][j-1], Orig[i-1][j], Orig[i-1][j+1], Orig[i][j-1], Orig[i][j+1]);
+                    *Copia = calcula_beirada(referencia,Orig[i][j],Orig[i-1][j-1],Orig[i-1][j],Orig[i-1][j+1],Orig[i][j-1],Orig[i][j+1]);
                 }
                 else if(j==0)                       //COLUNA DA ESQUERDA
                     {
                     referencia = 4;
-                    Copia[i][j] = calcula_beirada(referencia,Orig[i][j],Orig[i-1][j],Orig[i-1][j+1],Orig[i][j+1],Orig[i+1][j],Orig[i+1][j+1]);
+                    *Copia = calcula_beirada(referencia,Orig[i][j],Orig[i-1][j],Orig[i-1][j+1],Orig[i][j+1],Orig[i+1][j],Orig[i+1][j+1]);
                     }
                 else if(j==coluna-1)                //COLUNA DA DIREITA
                     {
                         referencia = 6;
-                        Copia[i][j] = calcula_beirada(referencia,Orig[i][j],Orig[i-1][j-1],Orig[i-1][j],Orig[i][j-1],Orig[i+1][j-1],Orig[i+1][j]);
+                        *Copia = calcula_beirada(referencia,Orig[i][j],Orig[i-1][j-1],Orig[i-1][j],Orig[i][j-1],Orig[i+1][j-1],Orig[i+1][j]);
                     }
                 }
 
             // CONDIÇÃO 3: VARIAVEL NO MEIO DA MATRIZ
             else    //condição 3 não precisa passar a variavel referencia porque todos os pontos em volta estão preenchidos
-            Copia[i][j]=calcula_meio(Orig[i][j],Orig[i-1][j-1],Orig[i-1][j],Orig[i-1][j+1],Orig[i][j-1],Orig[i][j+1],Orig[i+1][j-1],Orig[i+1][j],Orig[i+1][j+1]);
+            *Copia = calcula_meio(Orig[i][j],Orig[i-1][j-1],Orig[i-1][j],Orig[i-1][j+1],Orig[i][j-1],Orig[i][j+1],Orig[i+1][j-1],Orig[i+1][j],Orig[i+1][j+1]);
             
-        }
-    }
-
-    // escrita da nova matriz no terminal   (opcional)
-    printf("\n\nMATRIZ NO FILTRO LBP:");
-    printf("\n%s", tipo);
-    printf("\n%d %d", coluna, linha);
-    printf("\n%d", vMAX);
-    
-    for(i=0; i<linha; i++)
-    {
-        for(j=0; j<coluna; j++) 
-        {
-            printf("%d ", Copia[i][j]);
-        }
-        printf("\n");
-    }
-
-    // escrita da variante, linhas e colunas e valor maximo do pixel no arquivo.pgm
-    fprintf(arquivo, "%s\n", tipo);
-    fprintf(arquivo, "%d %d\n", coluna, linha);
-    fprintf(arquivo, "%d\n", vMAX);
-
-    // escrita da matriz no arquivo.pgm
-    for(i=0; i<linha; i++)
-    {
-        for(j=0; j<coluna; j++) 
-        {
-            fprintf(arquivo, "%d ", Copia[i][j]);
+            // colocando o pixel no arquivo
+            fprintf(arquivo, "%d ", *Copia);
+            // subindo o ponteiro para a nova posição
+            Copia++;
         }
         fprintf(arquivo, "\n");
     }
 
+    Copia = comecoCopia;    // voltando o ponteiro para a posição orginal
+
+    if(mostraNoTerminal)    // mostrando a matriz copia no terminal
+    {
+        printf("\n\nMATRIZ NO FILTRO LBP:");
+        printf("\n%s", tipo);
+        printf("\n%d %d", coluna, linha);
+        printf("\n%d\n", vMAX);
+        
+        for(i=0; i<linha; i++)
+        {
+            for(j=0; j<coluna; j++) 
+            {
+                printf("%d ", *Copia);
+                Copia++;
+            }
+            printf("\n");
+        }
+
+        Copia = comecoCopia;    // voltando o ponteiro para a posição orginal
+    }    
+    
+    // desalocando o ponteiro
+    free(Copia);
+
     // fechando o arquivo
     fclose(arquivo);
 
-    //fim do programa :)
+    // fim do programa :)
     return 0;
 }
